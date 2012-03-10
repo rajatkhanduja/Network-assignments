730ebbbcf26a84157e1e2f40eb92a9c664e6592d
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-03-10 14:28:39 +0530

    Added FTP_Server_Client. Barely started
    Changing language from C to C++.

---
+++ FTP_Server_Client/common/common.h
@@ -0,0 +1,6 @@
+/* Constants required in both files */
+#define SERVER_PORT 8765
+
+/* Functions defined using macros */
+#define ARG_ERROR_MSG(string) fprintf (stderr,"%s : " string, argv[0], optarg);
+
