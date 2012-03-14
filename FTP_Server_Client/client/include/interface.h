0733698a06c2e420b0c1a39de91cac40be877037
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-03-14 21:47:10 +0530

    Added files for client-side interface.

---
+++ FTP_Server_Client/client/include/interface.h
@@ -0,0 +1,10 @@
+/* This file declares the interface related functions and variables. */
+#ifndef CLIENT_INTERFACE_INCLUDED
+#define CLIENT_INTERFACE_INCLUDED
+
+/* Function to handle the input and execute the command requested by the user
+ * Space acts like a delimiter unless it is preceded by a 'backslash'. */
+void handleInput (string& userInput);
+
+
+#endif // End of file
---
+++ FTP_Server_Client/client/src/interface.cpp
@@ -0,0 +1,13 @@
+// This file defines the functions required for the functions and variables.
+
+#include <interface.h>
+
+
+void handleInput (string& userInput)
+{
+  // Separate the command string and the arguments.
+
+  // Use the command string to call respective functions.
+
+  // Print the value or the error message that is returned.
+}
