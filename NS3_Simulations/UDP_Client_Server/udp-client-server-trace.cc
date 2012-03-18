d68e2e813ac663031392baf24437e02e7c5c3d96
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-03-18 18:39:31 +0530

    Added the trace file and the required code.

--- NS3_Simulations/UDP_Client_Server/udp-client-server-trace.cc
+++ NS3_Simulations/UDP_Client_Server/udp-client-server-trace.cc
@@ -14,88 +14,86 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-// Network topology
-//
-//       n0    n1
-//       |     |
-//       =======
-//         LAN
-//
-// - UDP flows from n0 to n1
-
 #include <fstream>
 #include "ns3/core-module.h"
-#include "ns3/core-module.h"
 #include "ns3/csma-module.h"
+#include "ns3/point-to-point-helper.h"
 #include "ns3/applications-module.h"
 #include "ns3/internet-module.h"
-#include "ns3/trace-helper.h"
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("UdpClientServerExample");
+NS_LOG_COMPONENT_DEFINE ("UdpEchoExample");
 
-int
+int 
 main (int argc, char *argv[])
 {
 //
-// Enable logging for UdpClient and
+// Users may find it convenient to turn on explicit debugging
+// for selected modules; the below lines suggest how to do this
 //
-  LogComponentEnable ("UdpClient", LOG_LEVEL_INFO);
-  LogComponentEnable ("UdpServer", LOG_LEVEL_INFO);
-
+  LogComponentEnable ("UdpEchoExample", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_ALL);
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_ALL);
 //
-// Explicitly create the nodes required by the topology (shown above).
+// Allow the user to override any of the defaults and the above Bind() at
+// run-time, via command-line arguments
+//
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
+//
+// Explicitly create the nodes n1 and n2.
 //
   NS_LOG_INFO ("Create nodes.");
-  NodeContainer n;
-  n.Create (2);
+  Node n1, n2;
 
   InternetStackHelper internet;
-  internet.Install (n);
+  internet.Install (&n1);
+  internet.Install (&n2);
 
   NS_LOG_INFO ("Create channels.");
 //
-// Explicitly create the channels required by the topology (shown above).
+// Create a point-to-point channel between n1 and n2.
 //
-  CsmaHelper csma;
-  csma.SetChannelAttribute ("DataRate", DataRateValue (DataRate (5000000)));
-  csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
-  csma.SetDeviceAttribute ("Mtu", UintegerValue (1400));
-  NetDeviceContainer d = csma.Install (n);
-
+  PointToPointHelper pointToPoint;
+  NetDeviceContainer dc = pointToPoint.Install (&n1, &n2);
   Ipv4AddressHelper ipv4;
 //
 // We've got the "hardware" in place.  Now we need to add IP addresses.
 //
   NS_LOG_INFO ("Assign IP Addresses.");
   ipv4.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer i = ipv4.Assign (d);
+  Ipv4InterfaceContainer i = ipv4.Assign (dc);
 
   NS_LOG_INFO ("Create Applications.");
 //
-// Create one udpServer applications on node one.
+// Create a UdpEchoServer application on node one.
 //
-  uint16_t port = 4000;
-  UdpServerHelper server (port);
-  ApplicationContainer apps = server.Install (n.Get (1));
+  uint16_t port = 9;  // well-known echo port number
+  UdpEchoServerHelper server (port);
+  ApplicationContainer apps = server.Install (&n1);
   apps.Start (Seconds (1.0));
   apps.Stop (Seconds (10.0));
 
 //
-// Create one UdpClient application to send UDP datagrams from node zero to
+// Create a UdpEchoClient application to send UDP datagrams from node zero to
 // node one.
 //
-  uint32_t MaxPacketSize = 1024;
-  Time interPacketInterval = Seconds (0.05);
-  uint32_t maxPacketCount = 320;
-  UdpClientHelper client (i.GetAddress (1), port);
+  uint32_t packetSize = 1024;
+  uint32_t maxPacketCount = 1;
+  Time interPacketInterval = Seconds (1.);
+  UdpEchoClientHelper client (i.GetAddress (1), port);
   client.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
   client.SetAttribute ("Interval", TimeValue (interPacketInterval));
-  client.SetAttribute ("PacketSize", UintegerValue (MaxPacketSize));
-  apps = client.Install (n.Get (0));
+  client.SetAttribute ("PacketSize", UintegerValue (packetSize));
+  apps = client.Install (&n2);
   apps.Start (Seconds (2.0));
   apps.Stop (Seconds (10.0));
+
+// Enable tracing
+  AsciiTraceHelper ascii;
+  pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("udp-echo.tr"));
+  pointToPoint.EnablePcapAll ("udp-echo", false);
 
 //
 // Now, do the actual simulation.
